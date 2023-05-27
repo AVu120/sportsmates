7a2ae03ab01fd6c7ebbb688441fbda29261cfe65
Anthony Hien Vu <anthony.vu@checkbox.ai>
2023-05-28 08:18:08 +1000

    refactors.

--- src/pages/players/[id]/index.tsx
+++ src/pages/players/[id]/index.tsx
@@ -1,9 +1,7 @@
-import {} from "@radix-ui/react-icons";
 import { User } from "@supabase/supabase-js";
 import { createProxySSGHelpers } from "@trpc/react-query/ssg";
 import { GetServerSideProps } from "next";
 import Head from "next/head";
-import { useRouter } from "next/router";
 import superjson from "superjson";
 
 import buttonStyles from "@/_styles/_buttons.module.scss";
@@ -14,29 +12,14 @@ import { ProfilePicture } from "@/components/profile/ProfilePicture";
 import { appRouter } from "@/server/routers/_app";
 import { supabase } from "@/services/authentication";
 import { player } from "@/types/player";
-import useUser from "@/utils/hooks/useUser";
 import { formatLastSignInDate } from "@/utils/player";
 
 import styles from "./_index.module.scss";
-// const router = useRouter();
-// const { id } = router.query;
 
 interface ComponentProps {
   player: Omit<player, "birthday"> & { age: number };
   user: User | null;
 }
-
-// {
-//   "id": "5e334931-66b3-4f5d-8027-6bddbdde283c",
-//   "firstName": "Faker",
-//   "lastName": "Six",
-//   "skillLevel": "Intermediate",
-//   "gender": "Female",
-//   "age": 57,
-//   "city": "Camden Park",
-//   "description": "Ye to misery wisdom plenty polite to as. Prepared interest proposal it he exercise. My wishing an in attempt ferrars. Visited eat you why service looking engaged. At place no walls hopes rooms fully in. Roof hope shy tore leaf joy paid boy. Noisier out brought entered detract because sitting sir. Fat put occasion rendered off humanity has.",
-//   "lastSignIn": null
-// }
 
 const ProfilePage = ({ user, player }: ComponentProps) => {
   const formattedLastSignInDate = formatLastSignInDate(player.lastSignIn);
@@ -54,34 +37,52 @@ const ProfilePage = ({ user, player }: ComponentProps) => {
         <main className={styles.main}>
           {/* <div className={styles.profile_cards_container}> */}
           <div className={styles.profile_card}>
-            <div>
-              <h1>{`${player.firstName} ${player.lastName}`}</h1>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                rowGap: "1rem",
+              }}
+            >
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <h1>{`${player.firstName} ${player.lastName}`}</h1>
+                <div className={styles.button_container}>
+                  <button className={buttonStyles.primary_button}>
+                    Message
+                  </button>
+                </div>
+              </div>
               <div className={styles.profile_picture_container}>
                 <ProfilePicture height="200px" />
               </div>
               {player.lastSignIn && (
-                <p className={formStyles.label}>Last Active At</p>
+                <div>
+                  <p className={formStyles.label}>Last Active At</p>
+                  <p>{formattedLastSignInDate}</p>
+                </div>
               )}
-              {player.lastSignIn && <p>{formattedLastSignInDate}</p>}
-              <p className={formStyles.label}>Skill Level</p>
-              <p>{player.skillLevel}</p>
-              <p className={formStyles.label}>Gender</p>
-              <p>{player.gender}</p>
-              <p className={formStyles.label}>Age</p>
-              <p>{player.age}</p>
-              <p className={formStyles.label}>City</p>
-              <p>{player.city}</p>
-              <p className={formStyles.label}>Description</p>
-              <p>{player.description}</p>
-            </div>
-            <div className={styles.button_container}>
-              <button className={buttonStyles.primary_button}>Message</button>
+              <div>
+                <p className={formStyles.label}>Skill Level</p>
+                <p>{player.skillLevel}</p>
+              </div>
+              <div>
+                <p className={formStyles.label}>Gender</p>
+                <p>{player.gender}</p>
+              </div>
+              <div>
+                <p className={formStyles.label}>Age</p>
+                <p>{player.age}</p>
+              </div>
+              <div>
+                <p className={formStyles.label}>City</p>
+                <p>{player.city}</p>
+              </div>
+              <div>
+                <p className={formStyles.label}>Description</p>
+                <p>{player.description}</p>
+              </div>
             </div>
           </div>
-          {/* <div className={styles.profile_card}>
-              <p>Send them a message</p>
-            </div> */}
-          {/* </div> */}
         </main>
         <Footer />
       </div>

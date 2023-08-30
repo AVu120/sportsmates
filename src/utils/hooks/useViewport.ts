b1cb7b16997b1eddde3d6a8c5e5227f1b9e6a766
Anthony Hien Vu <anthonyvu@projexn.onmicrosoft.com>
2023-08-31 07:50:11 +1000

    Fix bug and change deploy region to Syd.

--- src/utils/hooks/useViewport.ts
+++ src/utils/hooks/useViewport.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "React";
+import { useEffect, useState } from "react";
 
 export const useViewport = () => {
   const [isRunningOnClient, setIsRunningOnClient] = useState(false);
